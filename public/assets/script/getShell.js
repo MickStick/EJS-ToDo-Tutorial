92328a0b3c1ce409df2be8e7a0188fd4f017cfe9
MickStick <mickstick01@hotmail.com>
2020-05-30 10:40:18 -0400

    removed unecessaery js files

--- public/assets/script/getShell.js
+++
@@ -1,2 +0,0 @@
-var getShell = require('shelljs');
-module.exports = getShell;
--- public/assets/script/shell.js
+++
@@ -1,7 +0,0 @@
-var ip = require('ip');
-var os = require('os');
-console.log('Server IP: ' + ip.address());
-var net = os.hostname();
-console.dir(net);
-//alert(net);
-//console.log(require('shelljs').exec('hostname'));
